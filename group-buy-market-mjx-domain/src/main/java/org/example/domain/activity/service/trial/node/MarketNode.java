34797b66d95355b5f40f54e45aa544ddd9353573
Mjx <12773018+Jack_mj14@user.noreply.gitee.com>
2025-02-18 17:17:02 +0800

    first-6

--- group-buy-market-mjx-domain/src/main/java/org/example/domain/activity/service/trial/node/MarketNode.java
+++ group-buy-market-mjx-domain/src/main/java/org/example/domain/activity/service/trial/node/MarketNode.java
@@ -6,14 +6,19 @@ import org.example.domain.activity.model.entity.MarketProductEntity;
 import org.example.domain.activity.model.entity.TrialBalanceEntity;
 import org.example.domain.activity.model.valobj.GroupBuyActivityDiscountVO;
 import org.example.domain.activity.model.valobj.SkuVO;
+import org.example.domain.activity.service.discount.IDiscountCalculateService;
 import org.example.domain.activity.service.trial.factory.DefaultActivityStrategyFactory;
 import org.example.domain.activity.service.trial.AbstractGroupBuyMarketSupport;
 import org.example.domain.activity.service.trial.thread.QueryGroupBuyActivityDiscountVOThreadTask;
 import org.example.domain.activity.service.trial.thread.QuerySkuVOFromDBThreadTask;
+import org.example.types.enums.ResponseCode;
+import org.example.types.exception.AppException;
 import org.example.types.framework.tree.StrategyHandler;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.util.Map;
 import java.util.concurrent.*;
 
 @Slf4j
@@ -26,6 +31,9 @@ public class MarketNode extends AbstractGroupBuyMarketSupport<MarketProductEntit
 
     @Resource
     private EndNode endNode;
+
+    @Resource
+    private Map<String, IDiscountCalculateService> discountCalculateServiceMap;
 
     @Override
     protected void multiThread(MarketProductEntity requestParameter, DefaultActivityStrategyFactory.DynamicContext dynamicContext) throws ExecutionException, InterruptedException, TimeoutException {
@@ -49,7 +57,18 @@ public class MarketNode extends AbstractGroupBuyMarketSupport<MarketProductEntit
     @Override
     public TrialBalanceEntity doApply(MarketProductEntity requestParameter, DefaultActivityStrategyFactory.DynamicContext dynamicContext) throws Exception {
         log.info("拼团商品查询试算服务-MarketNode userId:{} requestParameter:{}", requestParameter.getUserId(), JSON.toJSONString(requestParameter));
-
+        GroupBuyActivityDiscountVO groupBuyActivityDiscountVO = dynamicContext.getGroupBuyActivityDiscountVO();
+        TrialBalanceEntity trialBalanceEntity = new TrialBalanceEntity();
+        GroupBuyActivityDiscountVO.GroupBuyDiscount groupBuyDiscount = groupBuyActivityDiscountVO.getGroupBuyDiscount();
+        SkuVO skuVO = dynamicContext.getSkuVO();
+        IDiscountCalculateService discountCalculateService = discountCalculateServiceMap.get(groupBuyDiscount.getMarketPlan());
+        if (null == discountCalculateService) {
+            log.info("不存在{}类型的折扣计算服务，支持类型为:{}", groupBuyDiscount.getMarketPlan(), JSON.toJSONString(discountCalculateServiceMap.keySet()));
+            throw new AppException(ResponseCode.E0001.getCode(), ResponseCode.E0001.getInfo());
+        }
+// 折扣价格
+        BigDecimal deductionPrice = discountCalculateService.calculate(requestParameter.getUserId(), skuVO.getOriginalPrice(), groupBuyDiscount);
+        dynamicContext.setDeductionPrice(deductionPrice);
         return router(requestParameter, dynamicContext);
     }
 
